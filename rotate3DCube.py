3d9ce7d3c13a3e05f0c805536e2cc7aff7687464
cifurin <cifurin@hotmail.com>
2024-04-06 10:50:38 +0100

    minor updates

--- rotate3DCube.py
+++ rotate3DCube.py
@@ -4,6 +4,7 @@ from math import cos, sin, pi
 
 pygame.init()
 
+
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 RED = (255, 0, 0)
@@ -11,7 +12,7 @@ RED = (255, 0, 0)
 WIDTH, HEIGHT = 800, 600
 
 scale = 100
-origin = [WIDTH/2, HEIGHT/2]
+origin = [WIDTH / 2, HEIGHT / 2]
 
 angle = 0
 
@@ -30,46 +31,35 @@ vertices[6] = np.matrix([[1], [-1], [-1]])
 vertices[7] = np.matrix([[-1], [-1], [-1]])
 vertices[8] = np.matrix([[-0.5], [1], [1]])
 
-projection_matrix = np.matrix([
-    [1, 0, 0],
-    [0, 1, 0]
-])
+projection_matrix = np.matrix([[1, 0, 0], [0, 1, 0]])
 
 
 def generate_rot_x(theta):
     # theta MUST be in Radians
-    return np.matrix([
-        [1, 0, 0],
-        [0, cos(theta), -sin(theta)],
-        [0, sin(theta), cos(theta)]
-    ])
+    return np.matrix(
+        [[1, 0, 0], [0, cos(theta), -sin(theta)], [0, sin(theta), cos(theta)]]
+    )
 
 
 while True:
-    clock.tick(10)
+    clock.tick(60)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
 
-    rot_z = np.matrix([
-        [cos(angle), -sin(angle), 0],
-        [sin(angle), cos(angle), 0],
-        [0, 0, 1]
-    ])
+    rot_z = np.matrix(
+        [[cos(angle), -sin(angle), 0], [sin(angle), cos(angle), 0], [0, 0, 1]]
+    )
 
-    rot_y = np.matrix([
-        [cos(angle), 0, sin(angle)],
-        [0, 1, 0],
-        [-sin(angle), 0, cos(angle)]
-    ])
+    rot_y = np.matrix(
+        [[cos(angle), 0, sin(angle)], [0, 1, 0], [-sin(angle), 0, cos(angle)]]
+    )
 
-    rot_x = np.matrix([
-        [1, 0, 0],
-        [0, cos(angle), -sin(angle)],
-        [0, sin(angle), cos(angle)]
-    ])
+    rot_x = np.matrix(
+        [[1, 0, 0], [0, cos(angle), -sin(angle)], [0, sin(angle), cos(angle)]]
+    )
 
     # print(vertices[0])
     # print(np.dot(rot_x, vertices[0]))
@@ -85,19 +75,19 @@ while True:
 
     for point in vertices:
         #
-        rotated = np.dot(rot_y, point)
+        rotated = np.dot(rot_x, point)
 
         # This rotation is 180 degrees about the x-axis to account for the x-y axis used by Pygame, where y is downwards +ve
-        #rotated = np.dot(generate_rot_x(pi), rotated)
+        # rotated = np.dot(generate_rot_x(pi), rotated)
         # This projects the 3D point on to the 2D surface, This is effectively just removing the z component
         projected2D = np.dot(projection_matrix, rotated)
         # print(projected2D)
 
-        x = int(projected2D[0][0]*scale) + origin[0]
-        y = int(projected2D[1][0]*scale) + origin[1]
+        x = int(projected2D[0][0] * scale) + origin[0]
+        y = int(projected2D[1][0] * scale) + origin[1]
 
-        #x = int(projected2D[0][0])
-        #y = int(projected2D[1][0])
+        # x = int(projected2D[0][0])
+        # y = int(projected2D[1][0])
 
         render_points.append((x, y))
 
@@ -105,19 +95,19 @@ while True:
     # pygame.draw.circle(window, BLACK, (x, y), 5)
 
     for x, y in render_points:
-        #pygame.draw.circle(window, BLACK, (x, y), 5)
+        pygame.draw.circle(window, BLACK, (x, y), 5)
 
-        #pygame.draw.line(window, RED, render_points[4], render_points[5])
-        #pygame.draw.line(window, RED, render_points[5], render_points[6])
-        #pygame.draw.line(window, RED, render_points[6], render_points[7])
-        #pygame.draw.line(window, RED, render_points[7], render_points[4])
-        #pygame.draw.line(window, RED, render_points[0], render_points[4])
-        #pygame.draw.line(window, RED, render_points[1], render_points[5])
-        ##pygame.draw.line(window, RED, render_points[2], render_points[6])
-        #pygame.draw.line(window, RED, render_points[3], render_points[7])
-        #draw a diagonal line on face so that it can be identified
-        #pygame.draw.line(window, RED, render_points[0], render_points[2])
-        #pygame.draw.line(window, BLACK, render_points[5], render_points[7])
+        pygame.draw.line(window, RED, render_points[4], render_points[5])
+        pygame.draw.line(window, RED, render_points[5], render_points[6])
+        pygame.draw.line(window, RED, render_points[6], render_points[7])
+        pygame.draw.line(window, RED, render_points[7], render_points[4])
+        pygame.draw.line(window, RED, render_points[0], render_points[4])
+        pygame.draw.line(window, RED, render_points[1], render_points[5])
+        pygame.draw.line(window, RED, render_points[2], render_points[6])
+        pygame.draw.line(window, RED, render_points[3], render_points[7])
+        # draw a diagonal line on face so that it can be identified
+        # pygame.draw.line(window, RED, render_points[0], render_points[2])
+        # pygame.draw.line(window, BLACK, render_points[5], render_points[7])
 
         pygame.draw.line(window, RED, render_points[0], render_points[1])
         pygame.draw.line(window, RED, render_points[1], render_points[2])
